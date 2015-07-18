523468e7b1879daca3ef3a2fc4f2b25a06d2987d
Andy Smith <asmith@mapaction.org>
2015-07-18 12:58:06 +0100

    Dependancies via Wheel files

--- mapproxy.bat
+++ mapproxy.bat
@@ -1,16 +1,44 @@
-call "C:\OSGeo4w64\bin\o4w_env.bat"
-IF NOT EXIST env (
-  easy_install virtualenv
-  virtualenv --system-site-packages env
-  mv env\Scripts\activate.bat env\Scripts\activate.bat.old
-  type bat_header.txt > env\Scripts\activate.bat
-  type env\Scripts\activate.bat.old >> env\Scripts\activate.bat
-  env\Scripts\activate
-  pip install pyreadline MapProxy ipython lxml pillow
-  cd mapactionproxy
-  mapproxy-util serve-develop osm.yaml
-) ELSE (
-  env\Scripts\activate
-  cd mapactionproxy
-  mapproxy-util serve-develop osm.yaml
-)
+rem call "C:\OSGeo4w64\bin\o4w_env.bat"
+set pydir=D:\work\custom-software-group\code\github\mapproxy\ppython\App
+
+%pydir%\python.exe get-pip.py
+
+rem IF NOT EXIST env (
+rem   "%pydir%\Scripts\easy_install.exe" virtualenv
+rem   "%pydir%\Scripts\virtualenv.exe" --system-site-packages env
+rem   move env\Scripts\activate.bat env\Scripts\activate.bat.old
+rem   type bat_header.txt > env\Scripts\activate.bat
+rem   type env\Scripts\activate.bat.old >> env\Scripts\activate.bat
+rem  env\Scripts\activate
+
+rem "Wheel" install files are available from 
+rem http://www.lfd.uci.edu/~gohlke/pythonlibs/#pil
+
+rem 64bit
+rem   "%pydir%\Scripts\pip.exe" install ipython-3.2.1-py27-none-any.whl
+rem   "%pydir%\Scripts\pip.exe" install Pillow-2.9.0-cp27-none-win_amd64.whl
+rem   "%pydir%\Scripts\pip.exe" install pyproj-1.9.4-cp27-none-win_amd64.whl
+rem   "%pydir%\Scripts\pip.exe" install lxml-3.4.4-cp27-none-win_amd64.whl
+rem   "%pydir%\Scripts\pip.exe" install Shapely-1.5.9-cp27-none-win_amd64.whl
+rem   "%pydir%\Scripts\pip.exe" install -r requirements-mapproxy.txt MapProxy
+
+rem 32bit
+  "%pydir%\Scripts\pip.exe" install ipython-3.2.1-py27-none-any.whl
+  "%pydir%\Scripts\pip.exe" install Pillow-2.9.0-cp27-none-win_amd64.whl
+  "%pydir%\Scripts\pip.exe" install pyproj-1.9.4-cp27-none-win32.whl
+  "%pydir%\Scripts\pip.exe" install lxml-3.4.4-cp27-none-win_amd64.whl
+  "%pydir%\Scripts\pip.exe" install Shapely-1.5.9-cp27-none-win32.whl
+  "%pydir%\Scripts\pip.exe" install -r requirements-mapproxy.txt MapProxy
+ 
+  
+  rem pip install pyreadline MapProxy ipython lxml pillow pyproj Shapely
+rem ) ELSE (
+rem  env\Scripts\activate
+rem )
+
+
+rem pushd  mapactionproxy
+rem mapproxy-util serve-develop osm.yaml
+rem popd
+
+
