db4a027d62ae00ff1cd247143755c43287e6d76c
Andy Smith <asmith@mapaction.org>
2015-07-18 14:57:55 +0100

    tidy up seperate 32/64 bit instructions

--- mapproxy.bat
+++ mapproxy.bat
@@ -1,8 +1,20 @@
 rem call "C:\OSGeo4w64\bin\o4w_env.bat"
-set pydir=D:\work\custom-software-group\code\github\mapproxy\ppython\App
 
+rem (Portable Python Setup)
+set pydir=D:\work\custom-software-group\code\github\mapproxy\ppython\App
+copy c:\Python27\get-pip.py %~dp0
 %pydir%\python.exe get-pip.py
 
+rem 32bit
+  "%pydir%\Scripts\pip.exe" install ipython-3.2.1-py27-none-any.whl
+  "%pydir%\Scripts\pip.exe" install Pillow-2.9.0-cp27-none-win32.whl
+  "%pydir%\Scripts\pip.exe" install pyproj-1.9.4-cp27-none-win32.whl
+  "%pydir%\Scripts\pip.exe" install lxml-3.4.4-cp27-none-win32.whl
+  "%pydir%\Scripts\pip.exe" install Shapely-1.5.9-cp27-none-win32.whl
+  "%pydir%\Scripts\pip.exe" install -r requirements-mapproxy.txt MapProxy
+
+
+rem (installed Python, VirtualEnv setup)
 rem IF NOT EXIST env (
 rem   "%pydir%\Scripts\easy_install.exe" virtualenv
 rem   "%pydir%\Scripts\virtualenv.exe" --system-site-packages env
@@ -22,13 +34,6 @@ rem   "%pydir%\Scripts\pip.exe" install lxml-3.4.4-cp27-none-win_amd64.whl
 rem   "%pydir%\Scripts\pip.exe" install Shapely-1.5.9-cp27-none-win_amd64.whl
 rem   "%pydir%\Scripts\pip.exe" install -r requirements-mapproxy.txt MapProxy
 
-rem 32bit
-  "%pydir%\Scripts\pip.exe" install ipython-3.2.1-py27-none-any.whl
-  "%pydir%\Scripts\pip.exe" install Pillow-2.9.0-cp27-none-win32.whl
-  "%pydir%\Scripts\pip.exe" install pyproj-1.9.4-cp27-none-win32.whl
-  "%pydir%\Scripts\pip.exe" install lxml-3.4.4-cp27-none-win32.whl
-  "%pydir%\Scripts\pip.exe" install Shapely-1.5.9-cp27-none-win32.whl
-  "%pydir%\Scripts\pip.exe" install -r requirements-mapproxy.txt MapProxy
  
   
   rem pip install pyreadline MapProxy ipython lxml pillow pyproj Shapely
